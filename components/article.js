6d31ee2f7f8d656cec0e465bceda9611c2f7619e
WuTong <mikewu246@gmail.com>
2017-08-06 20:54:03 -0400

    Carousel project

--- components/article.js
+++ components/article.js
@@ -1,3 +1,16 @@
 /**
  * Created by WuTong on 2017/8/6.
  */
+import React from 'react'
+
+class article extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+    }
+
+
+}
